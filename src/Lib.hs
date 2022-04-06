753db6d00a0e05e022720c9036c6892e2046b9e9
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-04-06 13:17:18 +0200

    Add readData Function

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,11 +12,31 @@ import System.FilePath
 getSearchQueries :: IO [String]
 getSearchQueries = do concatMap words <$> getArgs
 
+-- | Returns the first [numberOfLines] lines of Input Data
+readData :: FilePath -> Int -> IO [String]
+readData filePath numberOfLines = do
+  -- Read Contents of File
+  content <- readFile filePath
+
+  -- Split Content of File to Lines
+  let linesOfFiles = Data.String.lines content
+
+  -- Return the first [numberOfLines] Lines of Data
+  return (Prelude.take numberOfLines linesOfFiles)
+
 project :: IO ()
 project = do
   -- Fetch Search Queries from CLI
   searchQueries <- getSearchQueries
 
   print searchQueries
+
+  -- Get Current Work Directory
+  cwd <- getCurrentDirectory
+
+  -- Read Input Data File
+  inputData <- readData (joinPath [cwd, "data", "data.json"]) 200
+
+  print inputData
 
   return ()
