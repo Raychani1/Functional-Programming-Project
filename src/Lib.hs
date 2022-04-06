d0d550f68537c5fe73f77e97212781e1c034cd64
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-04-06 15:58:33 +0200

    Add data loading to map functionality

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,26 +5,36 @@ module Lib
   )
 where
 
+import Data.Aeson
+import Data.ByteString.Lazy.Char8
+import Data.Map
+import Data.Maybe
 import Data.String
+import GHC.Generics
 import System.Directory
 import System.Environment
 import System.FilePath
 
 -- | Returns List of Search Queries
 getSearchQueries :: IO [String]
-getSearchQueries = do concatMap words <$> getArgs
+getSearchQueries = do Prelude.concatMap Data.String.words <$> getArgs
 
 -- | Returns the first [numberOfLines] lines of Input Data
 readData :: FilePath -> Int -> IO [String]
 readData filePath numberOfLines = do
   -- Read Contents of File
-  content :: String <- readFile filePath
+  content :: String <- Prelude.readFile filePath
 
   -- Split Content of File to Lines
   let linesOfFiles :: [String] = Data.String.lines content
 
   -- Return the first [numberOfLines] Lines of Data
   return (Prelude.take numberOfLines linesOfFiles)
+
+-- | Converts Maybe Map to Just Map
+maybeMapToMap :: Maybe (Map k v) -> Map k v
+maybeMapToMap (Just myMap) = myMap
+maybeMapToMap Nothing = Data.Map.empty
 
 project :: IO ()
 project = do
@@ -39,6 +49,8 @@ project = do
   -- Read Input Data File
   inputData :: [String] <- readData (joinPath [cwd, "data", "data.json"]) 200
 
-  print inputData
+  let dataMap :: Map String String = maybeMapToMap (Data.Aeson.decode (pack (Prelude.head inputData)) :: Maybe (Map String String))
+
+  print (dataMap ! "url")
 
   return ()
